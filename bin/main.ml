d2274ed678f0555fd388963c51348e48a4a57e72
jalsol <jalsol@pm.me>
2025-05-23 22:57:50 +0700

    feat: implement echo server
    https://dev.realworldocaml.org/concurrent-programming.html#examples-an-echo-server

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,26 @@
-let () = print_endline "Hello, World!"
+open Core
+open Async
+
+let run ~uppercase ~port =
+  let host_and_port =
+    Tcp.Server.create
+      ~on_handler_error:`Raise
+      (Tcp.Where_to_listen.of_port port)
+      (fun _addr r w ->
+         Pipe.transfer
+           (Reader.pipe r)
+           (Writer.pipe w)
+           ~f:(if uppercase then String.uppercase else Fn.id))
+  in
+  ignore host_and_port;
+  Deferred.never ()
+;;
+
+let () =
+  [%map_open.Command
+    let uppercase = flag "-uppercase" no_arg ~doc:"Convert to uppercase"
+    and port = flag "-port" (optional_with_default 6969 int) ~doc:"Port (default 6969)" in
+    fun () -> run ~uppercase ~port]
+  |> Command.async ~summary:"Echo server"
+  |> Command_unix.run
+;;
