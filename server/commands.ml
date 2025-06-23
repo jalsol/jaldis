d546e64b33612b39d4eab9008a669c897abd5159
jalsol <jalsol@pm.me>
2025-06-23 12:23:37 +0700

    refactor(server): unwrap args from R.Array of R.Bulk_string to string list
    protocol specifies that arguments are an array of bulk strings

--- server/commands.ml
+++ server/commands.ml
@@ -1,4 +1,4 @@
-open! Core
+open Core
 open Resp
 
 let hello =
@@ -12,14 +12,13 @@ let hello =
 
 let ping = function
   | [] -> R.String "PONG"
-  | [ R.String msg ] -> R.Bulk_string msg
-  | [ R.Bulk_string msg ] -> R.Bulk_string msg
+  | [ msg ] -> R.Bulk_string msg
   | _ -> R.Error "ERR wrong argument"
 ;;
 
 let set = function
   | [] | [ _ ] -> R.Error "ERR not enough arguments"
-  | [ R.Bulk_string key; R.Bulk_string value ] ->
+  | [ key; value ] ->
     Storage.set ~key ~value;
     R.String "OK"
   | _ -> R.Error "ERR not implemented"
@@ -27,7 +26,7 @@ let set = function
 
 let get = function
   | [] -> R.Error "ERR not enough arguments"
-  | [ R.Bulk_string key ] ->
+  | [ key ] ->
     let data = Storage.get ~key in
     (match data with
      | None -> R.Null
@@ -36,19 +35,26 @@ let get = function
   | _ -> R.Error "ERR not implemented"
 ;;
 
-let run_command cmd args =
-  match cmd with
-  | R.Bulk_string "PING" -> ping args
-  | R.Bulk_string "HELLO" -> hello
-  | R.Bulk_string "SET" -> set args
-  | R.Bulk_string "GET" -> get args
+let run_command ~args = function
+  | "PING" -> ping args
+  | "HELLO" -> hello
+  | "SET" -> set args
+  | "GET" -> get args
   | _ -> R.Error "ERR Not implemented"
 ;;
 
+let is_all_bulk_strings arr =
+  List.map arr ~f:(function
+    | R.Bulk_string s -> Ok s
+    | _ -> Error "ERR all args have to be bulk string")
+  |> Result.all
+;;
+
 let handle_msg = function
-  | R.Array args ->
-    (match args with
-     | [] -> R.Error "ERR Empty arguments"
-     | cmd :: rest -> run_command cmd rest)
+  | R.Array arr ->
+    (match is_all_bulk_strings arr with
+     | Error e -> R.Error e
+     | Ok [] -> R.Error "ERR Empty arguments"
+     | Ok (cmd :: args) -> run_command cmd ~args)
   | _ -> R.Error "ERR RESP requires an array of bulk string"
 ;;
